b1034b221ca87c0ff7ede9d7c26f3415e83a2e37
Joel Rudsberg <rudsberg@live.se>
2020-05-01 16:58:23 +0200

    Localized people card

--- src/components/People.js
+++ src/components/People.js
@@ -7,6 +7,8 @@ import SubHeader from "./SubHeader"
 import PeopleCard from "./PeopleCard"
 import Section from "./Section"
 import colors from "../styles/colors"
+import LocalizedStrings from "react-localization"
+import translation from "../translations/people"
 
 const PeopleContainer = styled.div`
   display: flex;
@@ -16,16 +18,14 @@ const PeopleContainer = styled.div`
 `
 
 export default ({ peopleImagesData }) => {
+  const strings = new LocalizedStrings(translation)
+
   return (
     <Section backgroundColor={colors.white}>
       <ContentWrapper>
-        <SectionTop
-          dark
-          header="Teamet"
-        >
-        Möt teamet som består av ett gäng nära vänner med flera års erfarenhet av att jobba tillsammans.
-          </SectionTop>
-        
+        <SectionTop dark header={strings.title}>
+          {strings.subTitle}
+        </SectionTop>
         <PeopleContainer>
           <PeopleCard
             name="Niklas Gustafsson"
@@ -34,91 +34,98 @@ export default ({ peopleImagesData }) => {
                 ? peopleImagesData.NiklasImage.childImageSharp.fixed
                 : ""
             }
-            position="Grundare &amp; VD "
-            town="Göteborg, Sverige"
-            text="Som bolagets VD ser Niklas till att vi når vårt mål om att bli det bästa AR-företaget i landet. Då han studerar industriell ekonomi är han expert inom företagsstrategi och kundrelationer. Han ansvarar för att teamet alltid mår bra och presterar på toppen av sin förmåga. Vid frågor om företagets strategi och kunder är Niklas rätt person att fråga."
+            position={strings.niklas.position}
+            town={strings.niklas.city}
+            text={strings.niklas.description}
             email="niklas@innotactsoftware.se"
             linkedin="https://www.linkedin.com/in/niklas-gustafsson-93025a12a/"
           />
           <PeopleCard
             name="Joel Rudsberg"
             image={
-                peopleImagesData
-                  ? peopleImagesData.JoelImage.childImageSharp.fixed
-                  : ""
-              }            position="Grundare, projektledare &amp; utvecklare"
-            town="Göteborg, Sverige"
-            text="Som projektledare ansvarar Joel för att InnoTacts projekt blir klara i tid och uppfyller alla kundens krav. Exempelvis är Joel projektledare för Boråstapeters revolutionerande nya app, Wonderwall. Joel studerar IT-programmet på Chalmers och har studerat Software Engineering i USA. Joels IT-kunskaper är ovärderliga för företaget och han lägger stor tid på att fortsatt personlig utveckling. Vid frågor om specifika projekt är Joel rätt person att fråga."
+              peopleImagesData
+                ? peopleImagesData.JoelImage.childImageSharp.fixed
+                : ""
+            }
+            position={strings.joel.position}
+            town={strings.joel.city}
+            text={strings.joel.description}
             email="joel@innotactsoftware.se"
             linkedin="https://www.linkedin.com/in/joel-rudsberg/"
           />
           <PeopleCard
             name="Anton Claesson"
             image={
-                peopleImagesData
-                  ? peopleImagesData.AntonImage.childImageSharp.fixed
-                  : ""
-              }            position="Grundare &amp; AR-specialist"
-            town="Mölnlycke, Sverige"
-            text={`Som företagets AR-specialist ansvarar Anton för att innovativa AR-lösningar tas fram efter kundens behov. Antons problemlösningsförmåga och tävlingsinstinkt gör att han ger allt för att lösa kundernas utmaningar. Vid sidan om InnoTact studerar Anton Automation och Mekatronik med master i Data Science och AI på Chalmers. Vid frågor om vad som är möjligt att göra med augmented reality är Anton rätt person att fråga.`}
+              peopleImagesData
+                ? peopleImagesData.AntonImage.childImageSharp.fixed
+                : ""
+            }
+            position={strings.anton.position}
+            town={strings.anton.city}
+            text={strings.anton.description}
             email="anton@innotactsoftware.se"
             linkedin="https://www.linkedin.com/in/anton-claesson/"
           />
           <PeopleCard
             name="Casper Lindberg"
             image={
-                peopleImagesData
-                  ? peopleImagesData.CasperImage.childImageSharp.fixed
-                  : ""
-              }            position="Grundare, CTO &amp; utvecklare"
-            town="Göteborg, Sverige"
-            text="Som företagets CTO ansvarar Casper för företagets tekniska strategi. Som erfaren programmerare och blivande data scientist är Casper expert på att snabbt lära sig och anpassa sig till nya teknologier. Casper ser till att vårt arbetssätt är effektivt och anpassat till de senaste teknologierna. Vid tekniska frågor är Casper rätt person att fråga."
+              peopleImagesData
+                ? peopleImagesData.CasperImage.childImageSharp.fixed
+                : ""
+            }
+            position={strings.casper.position}
+            town={strings.casper.city}
+            text={strings.casper.description}
             email="casper@innotactsoftware.se"
             linkedin="https://www.linkedin.com/in/casperlindberg1997/"
           />
           <PeopleCard
             name="Carl Claesson"
             image={
-                peopleImagesData
-                  ? peopleImagesData.CarlImage.childImageSharp.fixed
-                  : ""
-              }            position="Grundare, CFO &amp; utvecklare"
-            town="Mölnlycke, Sverige"
-            text="Som företagets CFO har Carl det yttersta ansvaret vid ekonomiska beslut. Carl studerar Teknisk Fysik och bidrar med sin problemlösningsförmåga och programmeringskunskaper till företaget. Carls problemlösningsförmåga har lett till flera nya innovationer som banat för nya appar. Vid frågor om företagets ekonomi är Carl rätt person att fråga."
+              peopleImagesData
+                ? peopleImagesData.CarlImage.childImageSharp.fixed
+                : ""
+            }
+            position={strings.carl.position}
+            town={strings.carl.city}
+            text={strings.carl.description}
             email="carl@innotactsoftware.se"
             linkedin="https://www.linkedin.com/in/carl-claesson-29145a16a/"
           />
           <PeopleCard
             name="Fredrik Hernqvist"
             image={
-                peopleImagesData
-                  ? peopleImagesData.FredrikImage.childImageSharp.fixed
-                  : ""
-              }            position="IT-arkitekt, utvecklare &amp; rådgivare"
-            town="Stockholm, Sverige"
-            text="Som IT-arkitekt, utvecklare och rådgivare ansvarar Fredrik för att kvalitetssäkra koden samt höja kompetensen i teamet. Fredrik har jobbat på Google i London, Microsoft i Seattle och Google i Stockholm. Fredrik pluggar datateknik på KTH, har gjort utbyte i Kina på det prestigefulla universitetet Tsinghua, känt som ”the Harvard of China”. Dessutom är Fredrik tävlingsprogrammerare på elitnivå och har även tävlat och presterat på elitnivå i matematiktävlingar."
+              peopleImagesData
+                ? peopleImagesData.FredrikImage.childImageSharp.fixed
+                : ""
+            }
+            position={strings.fredrik.position}
+            town={strings.fredrik.city}
+            text={strings.fredrik.description}
             linkedin="https://www.linkedin.com/in/fredrik-hernqvist-529479104/"
           />
           <PeopleCard
             name="Oskar Grönqvist"
             image={
-                peopleImagesData
-                  ? peopleImagesData.OskarImage.childImageSharp.fixed
-                  : ""
-              }            position="Androidutvecklare med specialisering inom augmented reality"
-            town="Göteborg, Sverige"
-            text="Som androidutvecklare med specialisering inom augmented reality ansvarar Oskar för att utveckla appar med väl utvalda arkitektur-mönster och de senaste augmented reality- teknikerna. Oskar studerar till civilingenjör i datateknik på Chalmers och går nu första året på IT-mastern Software Engineering and Technology. Oskar har därför den senaste kunskapen som Chalmers undervisar färskt i minnet och är redo att revolutionera framtidens augmented reality-appar."
+              peopleImagesData
+                ? peopleImagesData.OskarImage.childImageSharp.fixed
+                : ""
+            }
+            position={strings.oskar.position}
+            town={strings.oskar.city}
+            text={strings.oskar.description}
             linkedin="https://www.linkedin.com/in/oskar-gr%C3%B6nqvist-54a35b182/"
           />
           <PeopleCard
             name="Melker Veltman"
             image={
-                peopleImagesData
-                  ? peopleImagesData.MelkerImage.childImageSharp.fixed
-                  : ""
-              }            position="Backend-utvecklare"
-            town="Göteborg, Sverige"
-            text="Som företagets främsta backend-ansvarig lägger Melker den tekniska grunden för att InnoTact ska fortsätta revolutionera befintliga och nya branscher. Med sin IT kandidat från Chalmers och flera års erfarenhet som backendutvecklare har Melker det som krävs för att axla det ansvaret."
+              peopleImagesData
+                ? peopleImagesData.MelkerImage.childImageSharp.fixed
+                : ""
+            }
+            position={strings.melker.position}
+            town={strings.melker.city}
+            text={strings.melker.description}
             linkedin="https://www.linkedin.com/in/melker-veltman/"
           />
         </PeopleContainer>
---
+++ src/translations/people.js
@@ -0,0 +1,108 @@
+export default {
+  en: {
+    title: "The Team",
+    subTitle:
+      "Meet the team that consists of a bunch of close friends with years of experience working together.",
+    niklas: {
+      position: "Founder and CEO",
+      city: "Gothenburg, Sweden",
+      description:
+        "As the company's CEO, Niklas ensures that we reach our goal of becoming the best AR company in the country. As he studies industrial economics, he is an expert in business strategy and customer relations. He is responsible to make sure that the team is always doing well and performing at the top of their ability. For questions about the company's strategy and customers, Niklas is the right person to ask.",
+    },
+    joel: {
+      position: "Founder, Tech Lead and Developer",
+      city: "Gothenburg, Sweden",
+      description:
+        "As tech lead, Joel is often in close contact with our customers. For example he has been the tech lead and project manager for Boråstapeters revolutionizing new app, Wonderwall. Joel has studied software engineering in the US for a year and is now studying at Chalmers. For questions about specific projects or anything related to computer science, Joel is the right person to ask.",
+    },
+    anton: {
+      position: "Founder, Developer and AR specialist",
+      city: "Gothenburg, Sweden",
+      description:
+        "As the company's AR specialist, Anton is responsible for developing innovative AR solutions meeting our clients’ needs. Thanks to Anton's problem-solving ability and competition- oriented mind, he gives everything to solve our clients’ challenges. Alongside InnoTact, Anton is studying Automation and Mechatronics with a master’s in data science and AI at Chalmers. For questions about what is possible to do with augmented reality, Anton is the right person to ask.",
+    },
+    casper: {
+      position: "Founder, CTO and Developer",
+      city: "Gothenburg, Sweden",
+      description:
+        "As the company's CTO, Casper is responsible for the InnoTact’s technical strategy. As an experienced programmer, electrical engineer and a future data scientist, Casper is an expert at quickly learning and adapting to new technologies. Casper ensures that our working methods are effective and adapted to the latest technologies. For questions about the company's technical strategy or technical questions, Casper is the right person to ask.",
+    },
+    carl: {
+      position: "Founder, CFO and Developer",
+      city: "Gothenburg, Sverige",
+      description:
+        "As the company's CFO, Carl has the ultimate responsibility for financial decisions. Carl is studying Engineering Physics and contributes with his problem solving ability and programming skills to the company. Carl's problem-solving skills has led to several new innovations that have paved the way for new apps. For questions about the company's finances, Carl is the right person to ask.",
+    },
+    fredrik: {
+      position: "IT architect, Developer and Advisor",
+      city: "Stockholm, Sweden",
+      description:
+        'As an IT architect, developer and advisor, Fredrik is responsible for ensuring the quality of the code and increase the competence of the team. Fredrik has worked at Google in London, Microsoft in Seattle and Google in Stockholm. Fredrik is studying computer science at KTH, has made an exchange in China at the prestigious University of Tsinghua, known as "the Harvard of China". In addition, Fredrik has a history of being a competitive programmer at elite level and has also competed and performed at elite level in mathematics competitions.',
+    },
+    oskar: {
+      position: "Android Developer specializing in augmented reality",
+      city: "Gothenburg, Sweden",
+      description:
+        "As an android developer specializing in augmented reality, Oskar is responsible for developing apps with the correct architecture and the latest AR techniques. Oskar is studying his masters degree in software engineering at Chalmers. With the latest knowledge that Chalmers teaches, he is ready to revolutionize the future of augmented reality apps.",
+    },
+    melker: {
+      position: "Back-end Developer",
+      city: "Gothenburg, Sweden",
+      description:
+        "As InnoTacts primary responsible for the backend, Melker lays the technical foundation necessary for us to revolutionize existing and new industries. With his bachelor degree in software engineering from Chalmers and several years of experience within the field, Melker has what it takes to shoulder that responsibility.",
+    },
+  },
+  sv: {
+    title: "Teamet",
+    subTitle:
+      "Möt teamet som består av ett gäng nära vänner med flera års erfarenhet av att jobba tillsammans.",
+    niklas: {
+      position: "Grundare &amp; VD",
+      city: "Göteborg, Sverige",
+      description:
+        "Som bolagets VD ser Niklas till att vi når vårt mål om att bli det bästa AR-företaget i landet. Då han studerar industriell ekonomi är han expert inom företagsstrategi och kundrelationer. Han ansvarar för att teamet alltid mår bra och presterar på toppen av sin förmåga. Vid frågor om företagets strategi och kunder är Niklas rätt person att fråga.",
+    },
+    joel: {
+      position: "Grundare, projektledare &amp; utvecklare",
+      city: "Göteborg, Sverige",
+      description:
+        "Som projektledare ansvarar Joel för att InnoTacts projekt blir klara i tid och uppfyller alla kundens krav. Exempelvis är Joel projektledare för Boråstapeters revolutionerande nya app, Wonderwall. Joel studerar IT-programmet på Chalmers och har studerat Software Engineering i USA. Joels IT-kunskaper är ovärderliga för företaget och han lägger stor tid på att fortsatt personlig utveckling. Vid frågor om specifika projekt är Joel rätt person att fråga.",
+    },
+    anton: {
+      position: "Grundare &amp; AR-specialist",
+      city: "Göteborg, Sverige",
+      description:
+        "Som företagets AR-specialist ansvarar Anton för att innovativa AR-lösningar tas fram efter kundens behov. Antons problemlösningsförmåga och tävlingsinstinkt gör att han ger allt för att lösa kundernas utmaningar. Vid sidan om InnoTact studerar Anton Automation och Mekatronik med master i Data Science och AI på Chalmers. Vid frågor om vad som är möjligt att göra med augmented reality är Anton rätt person att fråga.",
+    },
+    casper: {
+      position: "Grundare, CTO &amp; utvecklare",
+      city: "Göteborg, Sverige",
+      description:
+        "Som företagets CTO ansvarar Casper för företagets tekniska strategi. Som erfaren programmerare och blivande data scientist är Casper expert på att snabbt lära sig och anpassa sig till nya teknologier. Casper ser till att vårt arbetssätt är effektivt och anpassat till de senaste teknologierna. Vid tekniska frågor är Casper rätt person att fråga.",
+    },
+    carl: {
+      position: "Grundare &amp; VD",
+      city: "Göteborg, Sverige",
+      description:
+        "Som företagets CFO har Carl det yttersta ansvaret vid ekonomiska beslut. Carl studerar Teknisk Fysik och bidrar med sin problemlösningsförmåga och programmeringskunskaper till företaget. Carls problemlösningsförmåga har lett till flera nya innovationer som banat för nya appar. Vid frågor om företagets ekonomi är Carl rätt person att fråga.",
+    },
+    fredrik: {
+      position: "IT-arkitekt, utvecklare &amp; rådgivare",
+      city: "Göteborg, Sverige",
+      description:
+        "Som IT-arkitekt, utvecklare och rådgivare ansvarar Fredrik för att kvalitetssäkra koden samt höja kompetensen i teamet. Fredrik har jobbat på Google i London, Microsoft i Seattle och Google i Stockholm. Fredrik pluggar datateknik på KTH, har gjort utbyte i Kina på det prestigefulla universitetet Tsinghua, känt som ”the Harvard of China”. Dessutom är Fredrik tävlingsprogrammerare på elitnivå och har även tävlat och presterat på elitnivå i matematiktävlingar.",
+    },
+    oskar: {
+      position: "Androidutvecklare med specialisering inom augmented reality",
+      city: "Göteborg, Sverige",
+      description:
+        "Som androidutvecklare med specialisering inom augmented reality ansvarar Oskar för att utveckla appar med väl utvalda arkitektur-mönster och de senaste augmented reality- teknikerna. Oskar studerar till civilingenjör i datateknik på Chalmers och går nu första året på IT-mastern Software Engineering and Technology. Oskar har därför den senaste kunskapen som Chalmers undervisar färskt i minnet och är redo att revolutionera framtidens augmented reality-appar.",
+    },
+    melker: {
+      position: "Grundare &amp; VD",
+      city: "Göteborg, Sverige",
+      description:
+        "Som företagets främsta backend-ansvarig lägger Melker den tekniska grunden för att InnoTact ska fortsätta revolutionera befintliga och nya branscher. Med sin IT kandidat från Chalmers och flera års erfarenhet som backendutvecklare har Melker det som krävs för att axla det ansvaret.",
+    },
+  },
+}
