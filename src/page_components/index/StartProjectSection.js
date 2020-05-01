d51ecd814c9c8e17ad87989108579e769f4aae56
Joel Rudsberg <rudsberg@live.se>
2020-05-01 14:31:02 +0200

    Created a translations folder

--- src/page_components/index/StartProjectSection.js
+++ src/page_components/index/StartProjectSection.js
@@ -8,6 +8,7 @@ import colors from "../../styles/colors"
 import styles from "../../styles/styles"
 import ContentBlob from "../../components/ContentBlob"
 import LocalizedStrings from "react-localization"
+import translation from "../../translations/start-project-section"
 
 const ServiceContainer = styled.div`
   display: flex;
@@ -35,42 +36,7 @@ const ContentBlobCustom = styled(ContentBlob)`
 `
 
 export default function StartProjectSection() {
-  const strings = new LocalizedStrings({
-    en: {
-      title: "Start a project",
-      topSection:
-        "Our team, which consists of motivated and driven developers, always strives to help you take your business to the next level. We work with close customer relationships to ensure that we meet all your needs.",
-      left: {
-        title: "Turn your idea into reality",
-        text:
-          "Do you already have an idea within Augmented Reality? Let's turn your idea into reality together.",
-        button: "START A PROJECT",
-      },
-      right: {
-        title: "Develop an idea together",
-        text:
-          "Not sure how Augmented Reality can be implemented in your business? Let’s evaluate your opportunities together.",
-        button: "READ MORE",
-      },
-    },
-    sv: {
-      title: "Starta projekt",
-      topSection:
-        "Vårt team, som består av motiverade och drivna utvecklare, strävar alltid efter att hjälpa ditt företag till nästa nivå. Vi arbetar med nära kundrelationer för att garantera att vi uppyller alla dina behov.",
-      left: {
-        title: "Realisera din idé",
-        text:
-          "Har du redan en idé inom Augmented Reality? Har du en produkt du vill förverkliga? Låt oss verkställa den tillsammans.",
-        button: "STARTA ETT PROJEKT",
-      },
-      right: {
-        title: "Vi skapar en idé tillsammans",
-        text:
-          "Är du osäker på hur Augmented Reality kan implementeras i ditt företag? Låt oss utvärdera dina möjligheter tillsammans.",
-        button: "LÄS MER",
-      },
-    },
-  })
+  const strings = new LocalizedStrings(translation)
 
   return (
     <Section backgroundColor={colors.white}>
---
+++ src/translations/start-project-section.js
@@ -0,0 +1,36 @@
+export default {
+  en: {
+    title: "Start a project",
+    topSection:
+      "Our team, which consists of motivated and driven developers, always strives to help you take your business to the next level. We work with close customer relationships to ensure that we meet all your needs.",
+    left: {
+      title: "Turn your idea into reality",
+      text:
+        "Do you already have an idea within Augmented Reality? Let's turn your idea into reality together.",
+      button: "START A PROJECT",
+    },
+    right: {
+      title: "Develop an idea together",
+      text:
+        "Not sure how Augmented Reality can be implemented in your business? Let’s evaluate your opportunities together.",
+      button: "READ MORE",
+    },
+  },
+  sv: {
+    title: "Starta projekt",
+    topSection:
+      "Vårt team, som består av motiverade och drivna utvecklare, strävar alltid efter att hjälpa ditt företag till nästa nivå. Vi arbetar med nära kundrelationer för att garantera att vi uppyller alla dina behov.",
+    left: {
+      title: "Realisera din idé",
+      text:
+        "Har du redan en idé inom Augmented Reality? Har du en produkt du vill förverkliga? Låt oss verkställa den tillsammans.",
+      button: "STARTA ETT PROJEKT",
+    },
+    right: {
+      title: "Vi skapar en idé tillsammans",
+      text:
+        "Är du osäker på hur Augmented Reality kan implementeras i ditt företag? Låt oss utvärdera dina möjligheter tillsammans.",
+      button: "LÄS MER",
+    },
+  },
+}
