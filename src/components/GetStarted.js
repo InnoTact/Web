6aa585aa4b776e599d6f76cf588e530daf397a0e
Joel Rudsberg <rudsberg@live.se>
2020-05-01 16:00:39 +0200

    Localized get started

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import SectionTop from './../components/SectionTop'
-import ContentWrapper from './../components/ContentWrapper'
-import Section from './../components/Section'
-import CenterContainer from './../components/CenterContainer'
-import colors from '../styles/colors'
-import Button from '../components/Button';
-import { Link } from 'gatsby'
+import React from "react"
+import SectionTop from "./../components/SectionTop"
+import ContentWrapper from "./../components/ContentWrapper"
+import Section from "./../components/Section"
+import CenterContainer from "./../components/CenterContainer"
+import colors from "../styles/colors"
+import Button from "../components/Button"
+import { Link } from "gatsby"
+import LocalizedStrings from "react-localization"
+import translation from "../translations/get-started"
 
-export default () => 
-<Section backgroundColor={colors.white}>
-    <ContentWrapper>
-    <SectionTop dark header={`Redo för att sätta igång?`}>Vi är här för att hjälpa dig. Klicka på knappen nedan för att nå oss. Tillsammans gör vi din idé till verklighet.</SectionTop>
-    <CenterContainer>
-        <Link to='/kontakt/#contact-form'>
-            <Button style={{marginTop: 25}} primary>Starta projekt</Button>
-        </Link>
-    </CenterContainer>
-    </ContentWrapper>
-</Section>
+export default () => {
+  const strings = new LocalizedStrings(translation)
+
+  return (
+    <Section backgroundColor={colors.white}>
+      <ContentWrapper>
+        <SectionTop dark header={strings.title}>
+          {strings.description}
+        </SectionTop>
+        <CenterContainer>
+          <Link to="/kontakt/#contact-form">
+            <Button style={{ marginTop: 25 }} primary>
+              {strings.button}
+            </Button>
+          </Link>
+        </CenterContainer>
+      </ContentWrapper>
+    </Section>
+  )
+}
---
+++ src/translations/get-started.js
@@ -0,0 +1,14 @@
+export default {
+  en: {
+    title: "Ready to get started?",
+    description:
+      "We are here to help you. Click the button below to reach us. Together we turn your idea into reality.",
+    button: "START PROJECT",
+  },
+  sv: {
+    title: "Redo för att sätta igång?",
+    description:
+      "Vi är här för att hjälpa dig. Klicka på knappen nedan för att nå oss. Tillsammans gör vi din idé till verklighet.",
+    button: "Starta projekt",
+  },
+}
