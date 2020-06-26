723b6a06d91cac41789cd5ed45d52a4b907acfc5
vltmn <melkerveltman@gmail.com>
2020-06-26 11:54:20 +0200

    add app store redirect

--- src/pages/app/borastapeter.js
+++ src/pages/app/borastapeter.js
@@ -1,32 +1,30 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react"
 
 const getMobileOperatingSystem = () => {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  
-        // Windows Phone must come first because its UA also contains "Android"
-      if (/windows phone/i.test(userAgent)) {
-          return "Windows Phone";
-      }
-  
-      if (/android/i.test(userAgent)) {
-          return "Android";
-      }
-  
-      // iOS detection from: http://stackoverflow.com/a/9039885/177710
-      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-          return "iOS";
-      }
-  
-      return "unknown";
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera
+  if (/windows phone/i.test(userAgent)) {
+    return "Windows Phone"
   }
-const Borastapeter = () => {
-    const [os, setOS] = useState('');
-    useEffect(() => {
-        setOS(getMobileOperatingSystem());
-    }, []);
-    
-    return <div>This should not be visible. You are viewing this from {os}</div>;
+
+  if (/android/i.test(userAgent)) {
+    return "Android"
+  }
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return "iOS"
+  }
+  return "unknown"
 }
 
-export default Borastapeter;
+const APP_STORE_LINK = 'https://apps.apple.com/se/app/wonderwall-ar-by-bor%C3%A5stapeter/id1478853508';
+const Borastapeter = () => {
+  useEffect(() => {
+    const os = getMobileOperatingSystem()
+    if (os === "iOS") {
+        window.location.href = APP_STORE_LINK;
+    }
+  }, [])
+
+  return <div>This should not be visible</div>
+}
+
+export default Borastapeter
