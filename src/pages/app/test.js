f7602573135cb3737e5a0f3fc2cb5fdbf9679e95
vltmn <melkerveltman@gmail.com>
2020-07-01 09:24:00 +0200

    add test app page

---
+++ src/pages/app/test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Test = () => (
+    <div>
+        <button onClick={() => window.location.href = 'https://innotactsoftware.com/app/borastapeter/?wallpaperID=3091'}>Öppna tapet</button>
+    </div>
+)
+
+export default Test;
