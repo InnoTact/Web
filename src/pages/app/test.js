d00ee4056094f6f17cd0e4aa43b6b185365730bd
vltmn <melkerveltman@gmail.com>
2020-07-02 13:28:01 +0200

    add regular link to open wallpaper for test

--- src/pages/app/test.js
+++ src/pages/app/test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 const Test = () => (
     <div>
-        <button onClick={() => window.location.href = 'https://innotactsoftware.com/app/borastapeter/?wallpaperID=3091'}>Öppna tapet</button>
+        <button onClick={() => window.location.href = 'https://innotactsoftware.com/app/borastapeter/?wallpaperID=3091'}>Öppna tapet (sätta location)</button>
+        <a href="/app/borastapeter/?wallpaperID=3091">Öppna tapet (länk)</a>
     </div>
 )
 
