b219b796c56b87c1058608d41c9c8a823a1f0644
vltmn <melkerveltman@gmail.com>
2020-07-02 13:33:45 +0200

    test universal link button

--- src/pages/app/test.js
+++ src/pages/app/test.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Test = () => (
     <div>
         <button onClick={() => window.location.href = 'https://innotactsoftware.com/app/borastapeter/?wallpaperID=3091'}>Öppna tapet (sätta location)</button>
-        <a href="/app/borastapeter/?wallpaperID=3091">Öppna tapet (länk)</a>
+        <a href="https://innotactsoftware.com/app/borastapeter/?wallpaperID=3091">Öppna tapet (länk)</a>
     </div>
 )
 
