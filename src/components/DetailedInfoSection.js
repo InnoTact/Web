7cefa5597aaba54cbf88b86a556a0e37deb8a8ec
Joel Rudsberg <rudsberg@live.se>
2021-04-04 14:08:36 +0200

    Mobile optimized

--- src/components/DetailedInfoSection.js
+++ src/components/DetailedInfoSection.js
@@ -7,6 +7,7 @@ import Header from "./Header"
 import SubHeader from "./SubHeader"
 import Text from "./Text"
 import styles from "../styles/styles"
+import { AppContext } from "./RootWrapper"
 
 export default function DetailedInfoSection({
   title,
@@ -30,10 +31,13 @@ export default function DetailedInfoSection({
     justify-content: center;
     max-width: 50%;
     margin: 0;
+    @media (max-width: ${styles.breakpoints.md + "px"}) {
+      max-width: unset;
+    }
   `
   const Image = styled.img`
     width: 100%;
-    margin: 0;
+    margin: 0 auto;
   `
   const DownloadAppContainer = styled.div``
   const halfInnerMargin = 20
@@ -44,27 +48,44 @@ export default function DetailedInfoSection({
       backgroundColor={light ? colors.white : colors.lightgrey}
       {...props}
     >
-      <ContentWrapper>
-        <Container>
-          <SideContainer style={{ paddingRight: halfInnerMargin }}>
-            <Header
-              dark
-              styles={{ color: colors.dark, textAlign: "left", marginLeft: 0 }}
-              text={title}
-            />
-            <SubHeader style={{ textAlign: "left" }} dark>
-              {subTitle}
-            </SubHeader>
-            <Text dark>{description}</Text>
-            <DownloadAppContainer></DownloadAppContainer>
-          </SideContainer>
-          <SideContainer
-            style={{ textAlign: "center", paddingLeft: halfInnerMargin }}
-          >
-            <Image src={imgSrc} />
-          </SideContainer>
-        </Container>
-      </ContentWrapper>
+      <AppContext.Consumer>
+        {value => {
+          return (<ContentWrapper>
+            <Container>
+              <SideContainer
+                style={{
+                  paddingRight:
+                    value.isTablet || value.isMobile ? 0 : halfInnerMargin,
+                }}
+              >
+                <Header
+                  dark
+                  styles={{
+                    color: colors.dark,
+                    textAlign: "left",
+                    marginLeft: 0,
+                  }}
+                  text={title}
+                />
+                <SubHeader style={{ textAlign: "left" }} dark>
+                  {subTitle}
+                </SubHeader>
+                <Text dark>{description}</Text>
+                <DownloadAppContainer></DownloadAppContainer>
+              </SideContainer>
+              <SideContainer
+                style={{
+                  textAlign: "center",
+                  paddingLeft:
+                    value.isTablet || value.isMobile ? 0 : halfInnerMargin,
+                }}
+              >
+                <Image src={imgSrc} />
+              </SideContainer>
+            </Container>
+          </ContentWrapper>)
+        }}
+      </AppContext.Consumer>
     </Section>
   )
 }
