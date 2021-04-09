95799d6fa9411327bfaf5e0fbea63e544741888b
Joel Rudsberg <rudsberg@live.se>
2021-04-09 12:00:51 +0200

    First iteration of texts

--- src/translations/wall-art-visualizer.js
+++ src/translations/wall-art-visualizer.js
@@ -2,43 +2,54 @@ export default {
   en: {
     seo: {
       title: `Wall Art Visualizer: Visualize Wall Art and Posters in AR`, // max 60 character
-      description: `With our Wallpaper Visualizer AR technology your customers can visualize and experience your wallpapers inside their homes, before buying and without ordering any samples.`, // max 160 TODO
+      description: `The Wall Art Visualizer lets your customers to visualize and experience your art and posters in their home in a simple, fun, and accurate way.`, // max 160
     },
     landing: {
       productName: "Wall Art Visualizer",
       shortPitch:
-        "With the Wallpaper Visualizer your customers can visualize and experience your wallpapers inside their homes before buying and without ordering any samples. It is the fastest way to experience all your collections of wallpapers, helping the customer find the wallpaper they want.",
-      videoAlt: "Wallpaper Visualizer using Augmented Reality",
+        "The Wall Art Visualizer lets your customers to visualize and experience your art and posters, allowing them to find the best wall art for their home in a simple, fun, and accurate way.",
+      videoAlt:
+        "Demonstration of how the Wall Art Visualizer works when finding your next artwork or poster for your home.",
     },
     detailedInfo: {
-      title: "Everything in one place",
+      title: "All Artworks in Your Pocket",
       subTitle:
-        "Browse wallpapers, test them in your room, share your favorite wallpapers, obtain measurements and place your order. All in one app.",
+        "Browse artworks and posters, test them in your room with different sizes and frames, build photo walls, and place your order. All in an app.",
       description:
-        "The Wallpaper Visualizer allows your customer to browse and experience all your collections of wallpapers in a fast and intuitive native app. Through the camera of the phone, the wallpapers are visualized in your customers' room while also allowing them to instantaneously switch between different wallpapers. The app will have your company name and be available as an iOS and Android app with your particular branding and feel.",
+        "The Art Visualizer allows your customers to try your art in their home in a fast and intuitive native app. Through the camera of the phone, the artworks are visualized in your customers' room while also allowing them to instantaneously switch motives, experiment with multiple pieces to find the perfect combination, and change frames and try differernt sizes. The app will have your company name and be available as an iOS app with your particular branding and feel.",
     },
     featureSection: {
       title: "Popular Features",
       features: [
         {
-          title: "Measure Tool and Sharing",
+          title: "Multiple Artworks Simultaneously",
           description:
-            "With exact AR wallpaper data, the number of rolls needed to paper the room is presented to the user. Users can also take an image of their papered room and share it with friends and family or on social media. ",
+            "Create entire photo-walls with any number of arworks to find your favorite combination. Trying out a specific combination of artworks has never been easier.",
+        },
+        {
+          title: "Change Frame and Color",
+          description:
+            "If you also sell frames, we create realistic 3D ones that can be applied to any artwork with different colors. Finding the perfect matching frame has never been easier.",
+        },
+        {
+          title: "Try All Sizes",
+          description:
+            "Change size of your artwork to any of the sizes that you sell your artworks in, we match what you use on your website.",
+        },
+        {
+          title: "Buy Artworks In-App",
+          description:
+            "The fastest integration option is to re-route the customer to the checkout page on your website when they want to order. But we also offer the fantastic possibility of making purchases in-app, which likely will create a surge in your sales.",
         },
         {
           title: "Filter, Search, and Sort",
           description:
-            "Filter, search, and sort through your wallpapers in a lightning-fast native iOS and Android app. The product page shows any desired wallpaper meta-data and links to the specific wallpaper on your website. All texts in the app are changed using our backend admin interface.",
+            "Filter, search, and sort through your wall-art in a lightning-fast native iOS. The product page shows any desired art meta-data and links to the specific product on your website. All texts in the app are changed using our backend admin interface.",
         },
         {
           title: "Website Integration",
           description:
-            "The app is integrated with your existing website to allow customers browsing your website to launch the AR experience to test the wallpaper directly in their room. App users can easily open any wallpaper on your website by clicking links in the app.",
-        },
-        {
-          title: "Custom Fit Murals",
-          description:
-            "All types of wallpapers are handled, not just repeating patterns. With the mural feature, customers can swipe to select the part of the mural they prefer in the AR wallpaper covering.",
+            "The app is integrated with your existing website to allow customers browsing your website to launch the AR experience to test the art-piece directly in their room. App users can easily open any artwork on your website by clicking links in the app.",
         },
         {
           title: "Rich User Statistics",
@@ -50,20 +61,11 @@ export default {
           description:
             "The app you obtain is highly personalized, matching your style guide and the design of your website. When kicking things off, we develop a design proposal where you have the possibility of suggesting new ideas and making changes.",
         },
-        {
-          title: "Buy Wallpapers In-App",
-          description:
-            "The fastest integration option is to re-route the customer to the checkout page on your website when they want to order. But we also offer the fantastic possibility of making purchases in-app, which likely will create a surge in your sales.",
-        },
-        {
-          title: "Multi-Language Support",
-          description:
-            "The apps support any number of languages you desire. With the admin interface, text changes and translations are instantly reflected on the users' apps. Prefer working in excel? We export to that too.",
-        },
+
         {
           title: "Push Notifications",
           description:
-            "Keep your customers updated with new collections, wallpapers, or other exciting information using push notifications. Use our ready-made templates or create a custom one using simple drag and drop. Schedule them, or target a specific customer segment. No matter if you send tens of thousands per week, they are completely free.",
+            "Keep your customers updated with new artworks and other exciting information using push notifications. Use our ready-made templates or create a custom one using simple drag and drop. Schedule them, or target a specific customer segment. No matter if you send tens of thousands per week, they are completely free.",
         },
       ],
     },
@@ -76,7 +78,7 @@ export default {
     takeAction: {
       title: "Let's do this together",
       description:
-        "Get ahead of your competition by joining the industry leaders to increase your customer experience, sales, and much more. Contact us and we will schedule a meeting. We would be thrilled to work with you.",
+        "This is an unique opportunity to get ahead of your competition by offering your customers a revolutionary way to experience your artworks from home. You will increase your sales, custom satisfaction, and much more. We are certain the future of selling art is deeply integrated in Augmented Reality. Contact us and we will schedule a meeting, we would be thrilled to work with you.",
       btnText: "Contact Us",
     },
   },
