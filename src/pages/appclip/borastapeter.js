f030c53f663a493072386639b36a2bb172b2bdc9
Joel Rudsberg <rudsberg@live.se>
2021-06-18 14:17:17 +0200

    Test for app clip page BT

---
+++ src/pages/appclip/borastapeter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function borastapeter() {
+    return (
+        <div>
+            Press the app banner above to launch the AR experience.
+        </div>
+    )
+}
