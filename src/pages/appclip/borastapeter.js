47f6bcd13aef3be860ed9fb3587cd8ddc0d8045a
Joel Rudsberg <rudsberg@live.se>
2021-06-21 08:46:47 +0200

    Borås app clip page

--- src/pages/appclip/borastapeter.js
+++ src/pages/appclip/borastapeter.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Helmet } from "react-helmet"
 
 export default function borastapeter() {
     return (
         <div>
+            <Helmet>
+                <title>Wonderwall AR By Boråstapeter</title>
+                <meta 
+                name="apple-itunes-app" 
+                content="app-clip-bundle-id=InnoTact.Borastapeter.Clip, app-id=N3QW3RP8Y5"
+                ></meta>
+            </Helmet>
+            <p>
             Press the app banner above to launch the AR experience.
+            </p>
         </div>
     )
 }
