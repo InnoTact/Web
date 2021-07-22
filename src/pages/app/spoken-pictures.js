9f0af6b12c6ad1956324eb2ddac7579abef26e42
Joel Rudsberg <rudsberg@live.se>
2021-07-22 15:29:07 +0200

    First version of email verification page

---
+++ src/pages/app/spoken-pictures.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled, { css } from "styled-components"
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    /*background-image: url("photographer.jpg");*/
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`
+
+export default function SpokenPictures() {
+    return (
+        <Container>
+            <h1>Spoken Pictures</h1>
+            <p>Press the link on your iPhone or iPad to sign in!</p>
+        </Container>
+    )
+}
