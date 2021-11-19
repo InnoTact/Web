fb31ee0752ddcbb309f2f977634e58f5b48c5190
vltmn <melkerveltman@gmail.com>
2021-11-19 17:49:59 +0100

    Add ostermalm champagne logo

---
+++ src/pages/app/ostermalm-champagne.js
@@ -0,0 +1,45 @@
+import { graphql } from 'gatsby'
+import React from 'react'
+import Img from "gatsby-image"
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    overflow: hidden;
+    flex-direction: column;
+    background-color: black;
+    align-items: center;
+    justify-content: center;
+`
+
+const ImageStyle = {
+    width: '100vw',
+    margin: '1.2rem 0'
+}
+
+const InnerImgStyle = {
+    'object-fit': 'contain'
+}
+export const pageQuery = graphql`
+    query {
+        EtiquetteImage: file(relativePath: {eq: "ostermalm_champagne.jpg"}) {
+            childImageSharp {
+                fluid(maxWidth: 1400) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+`
+
+export default function OstermalmChampagne({ data }) {
+    console.log(data);
+    return <Container>
+        <Img fluid={data.EtiquetteImage.childImageSharp.fluid} style={ImageStyle} imgStyle={InnerImgStyle} />
+        <div>
+            <audio controls autoPlay src="https://storage.googleapis.com/innotact-resources/spoken-pictures/audio/ostermalm-champagne.mp3">Unfortunately your browser does not support the feature to play this audio.</audio>
+        </div>
+    </Container>
+}
