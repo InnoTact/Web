d018b6fec484a03eda944cb58e67056a8df1f121
vltmn <melkerveltman@gmail.com>
2022-02-14 23:03:44 +0100

    add web visualizer page

---
+++ src/translations/web-visualizer.js
@@ -0,0 +1,42 @@
+export default {
+    en: {
+        seo: {
+            title: `Web Visualizer: Instant AR from your webshop`, // max 60 character
+            description: `With our Web Visualizer AR technology your customers can visualize your items inside their homes, compatible with Shopify, WooCommerce, PrestaShop and others.` // max 160
+        },
+        landing: {
+            productName: "Web Visualizer",
+            shortPitch: "Let your users test your posters on their wall, lamps in the ceiling or pots in their bookshelf with our Web Visualizer AR Technology."
+        },
+        detailedInfo: {
+            title: "Instant from your e-commerce",
+            subTitle: "The seamless experience from your webshop to their homes will give you an unprecedented marked advantage. Simple integration with your WooCommerce, Shopify, PrestaShop and others.",
+            description: "InnoTact Web Visualizer keeps it simple, because that is what your customers want. Through a simple click on your website, they can experience your item in their environment, whether it is wall art, household items or lights. According to Shopify, you increase your conversion rate by 94% by using augmented reality. Your customers will thank you. Stay on top of your game and one step ahead of your competition."
+        },
+        demo: {
+            title: "Try it out",
+            demos: [
+                {
+                    title: "Wall Art",
+                    description: "Visualize wall art in seconds with our Web Visualizer. Let your users experience your art in the size they want it, and with an accompanying frame.",
+                    models: {
+                        "30x40": "/3d/30x40_test.glb",
+                        "50x70": "/3d/50x70_test.glb"
+                    }
+                }
+            ]
+        }
+    },
+    sv: {
+        seo: {
+
+        },
+        landing: {
+            // productName: "Web Visualizer",
+            // shortPitch: "Låt era användare uppleva era posters på deras vägg, era lampor i taket eller era krukor på fönsterbänken med vår Web Visualizer, direkt i mobilen."
+        },
+        detailedInfo: {
+            
+        }
+    }
+}
